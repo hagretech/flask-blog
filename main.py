2407d5831b1bd956c8a321994440d088beeee716
kiztopia <kiztopia@Gmail.com>
2020-05-12 05:12:01 -0700

    base database model is done and data.db is created

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from flask import Flask , render_template , request ,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+## the sql database confic
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+## database models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20),nullable=False)
+    profile_img = db.Column(db.String(), default='main.jpg')
+    posts = db.relationship('Post', backref='auther',lazy=True)
+    def __repr__(self):
+        return self.name
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40),nullable=False)
+    content = db.Column(db.String(1000),nullable=False)
+    date = db.Column(db.DateTime,default=datetime.utcnow)
+    auther_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable= False)
+
+    def __repr__(self):
+        return self.title
+    
+    
 @app.route('/')
 def test():
     return 'hellow world'
